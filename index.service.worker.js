b5e841ec4877d1261581ea7facfd3dabaf583b5d
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-11 18:21:27 +1000

    export version with new icon and fix bug

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749624947|36360808513';
+const CACHE_VERSION = '1749629985|1289663127';
 /** @type {string} */
 const CACHE_PREFIX = 'scribble brawlr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
