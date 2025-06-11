b1348918f5aebcc01625ba7b81d476cc0642713e
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-11 21:32:29 +1000

    push new version

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749635829|649499240';
+const CACHE_VERSION = '1749641532|6352752547';
 /** @type {string} */
 const CACHE_PREFIX = 'scribble brawlr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
