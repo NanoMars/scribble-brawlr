6b40569b5d2130a250a23de6ec024019f0296d2b
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-11 19:44:15 +1000

    made a really obvious change to see if changes are being populated at all

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749634773|4606960426';
+const CACHE_VERSION = '1749635023|4856958385';
 /** @type {string} */
 const CACHE_PREFIX = 'scribble brawlr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
