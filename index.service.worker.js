69a12f2e3223530edf2c00296283d35b1c84e924
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-11 19:33:20 +1000

    changed something, lets see if it fixed the issue

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749633550|3530662092';
+const CACHE_VERSION = '1749634377|4211072697';
 /** @type {string} */
 const CACHE_PREFIX = 'scribble brawlr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
