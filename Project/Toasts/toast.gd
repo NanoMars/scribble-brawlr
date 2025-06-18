901ddd757ac4e814697f97a57319d81316f62f33
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-17 22:02:20 -0700

    toasts sorta work but i'm about to overhaul it

--- Project/Toasts/toast.gd
+++ Project/Toasts/toast.gd
@@ -1,31 +1,59 @@
 # toast.gd
 extends PanelContainer
 
+
+@onready var temp_thing: PackedScene = preload("res://Toasts/toast.tscn")
+
 @export var toast_label: Label
 var toast_string: String:
 	set(value):
 		toast_label.text = value
-var duration: float = 3.0
+
+func _ready() -> void:
+	modulate = Color(1, 1, 1, 1)
+
+var animation_duration: float = 0.3
+var duration: float = 5
 
 func show_toast() -> void:
+	modulate = Color(1, 1, 1, 1)
+	await get_tree().process_frame
+	
 	var tree = get_tree()
 	var container = get_parent()
-	var assigned_location = global_position
+	
+	var clone = temp_thing.instantiate()
+	
+	container.add_child(clone)
 	container.remove_child(self)
 	tree.root.add_child(self)
+	var assigned_location = clone.global_position
 	
 
+
 	global_position = Vector2(assigned_location.x, 1200)
-	toast_label.text = toast_string
 	#tween from current position to assigned_location
-	print("before_tween")
+
 	var tween = create_tween()
-	tween.tween_property(self, "global_position", assigned_location, duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "global_position", assigned_location, animation_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	await tween.finished
+	clone.queue_free()
 	print("after_tween")
 	container.add_child(self)
-	#disappear()
+
+
+	await get_tree().create_timer(duration).timeout
+	disappear()
 
 func disappear() -> void:
+	var tree = get_tree()
+	var initial_position = global_position
+	var off_screen_position = Vector2(initial_position.x, 1200)  # Assuming 1920/1080 resolution
+	
+	get_parent().remove_child(self)
+	tree.root.add_child(self)
 	var tween = get_tree().create_tween()
 	# tween from current position to off the screen
+	tween.tween_property(self, "global_position", off_screen_position, animation_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	await tween.finished
+	queue_free()
--- Project/Toasts/toast_manager.gd
+++ Project/Toasts/toast_manager.gd
@@ -4,7 +4,11 @@ extends Node
 @onready var toast_window_scene: PackedScene = preload("res://Toasts/toast_window.tscn")
 @onready var toast_scene: PackedScene = preload("res://Toasts/toast.tscn")
 
+var toast_duration: float = 100
+
 var toast_container: Node
+
+var toast_array: Array[Node] = []
 
 func _ready() -> void:
 	await get_tree().process_frame
@@ -12,12 +16,17 @@ func _ready() -> void:
 	get_tree().get_root().add_child(toast_instance)
 	toast_container = toast_instance.get_node("ToastContainer")
 
-	display_toast("Welcome to the game!")
+	
+	for i in range(500):
+		await get_tree().create_timer(0.3).timeout
+		display_toast("Toast number %d" % i)
 
 
 func display_toast(toast_string: String) -> void:
 	var toast_instance = toast_scene.instantiate()
-	toast_container.add_child(toast_instance)
+	get_tree().get_root().add_child(toast_instance)
+	toast_array.append(toast_instance)
+	toast_instance.duration = toast_duration
 	toast_instance.toast_string = toast_string
 	toast_instance.show_toast()
 	
