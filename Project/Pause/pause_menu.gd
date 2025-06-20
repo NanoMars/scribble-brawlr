cf812b47372215a5d6c2e687f016b19309acef8d
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-19 20:27:21 -0700

    pause menu can be closed with b

--- Project/Pause/pause_menu.gd
+++ Project/Pause/pause_menu.gd
@@ -5,7 +5,7 @@ var player_id: int
 
 var is_paused: bool = false
 
-var pause_menu_held_controller_ids: Array[int] = []
+var pause_menu_held_controller_ids: Dictionary[int, Array] = {}
 
 var time_scale_animation_duration: float = 0.15
 
@@ -13,18 +13,27 @@ var time_scale_start: float = 1.0
 var time_scale_end: float = 1.0
 var time_scale_start_time: float = 0.0
 
+@export var pause_button: int = JOY_BUTTON_START
+@export var back_button: int = JOY_BUTTON_B
+
+@export var buttons: Array[Button] = []
+
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _input(event):
 	if event is InputEventJoypadButton:
 		if event.pressed:
-			if event.button_index == JOY_BUTTON_START:
-				
-				var temp_controller_id = event.device
-				if controller_id in PlayerManager.joined_players and temp_controller_id not in pause_menu_held_controller_ids:
+			var temp_controller_id = event.device
 
+			if temp_controller_id not in pause_menu_held_controller_ids:
+					pause_menu_held_controller_ids[temp_controller_id] = []
+
+			if event.button_index == pause_button:
+				if controller_id in PlayerManager.joined_players and pause_button not in pause_menu_held_controller_ids[temp_controller_id]:
+					
 					var temp_player_id: int = PlayerManager.joined_players[temp_controller_id]
+
 					if !is_paused:
 						player_id = temp_player_id
 						controller_id = temp_controller_id
@@ -35,13 +44,27 @@ func _input(event):
 						ToastManager.display_toast("Only player " + str(player_id) + " can unpause.") 
 
 					ToastManager.display_toast("temp id " + str(temp_player_id) + "normal id" + str(player_id))
-					pause_menu_held_controller_ids.append(temp_controller_id)
+					
 				else:
 					ToastManager.display_toast("Player trying to pause is not in the game.")
+				
+			
+				if event.button_index not in pause_menu_held_controller_ids[temp_controller_id]:
+					pause_menu_held_controller_ids[temp_controller_id].append(event.button_index)
+			elif event.button_index == back_button:
+				if controller_id in PlayerManager.joined_players:
+					var temp_player_id: int = PlayerManager.joined_players[temp_controller_id]
+					if is_paused and temp_player_id == player_id and back_button not in pause_menu_held_controller_ids[temp_controller_id]:
+						hide_pause_menu()
+						ToastManager.display_toast("Unpaused game.")
+					elif is_paused and temp_player_id != player_id:
+						ToastManager.display_toast("Only player " + str(player_id) + " can unpause.")
 		elif not event.pressed:
-			if event.button_index == JOY_BUTTON_START:
-				var temp_controller_id = event.device
-				pause_menu_held_controller_ids.erase(temp_controller_id)
+			var temp_controller_id = event.device
+
+			if temp_controller_id in pause_menu_held_controller_ids and event.button_index in pause_menu_held_controller_ids[temp_controller_id]:
+				pause_menu_held_controller_ids[temp_controller_id].erase(event.button_index)
+			
 			
 
 @export var player_icon: TextureRect
